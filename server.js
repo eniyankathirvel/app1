f6beb051285f335ff4ebaeaf42968967607bfdb2
Eniyan Kathirvel <eniyankathirvel@Eniyans-MacBook-Pro.local>
2024-06-15 23:11:11 +0530

    Changing the message

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const ENV = 'DEV';
 const app = express();
 app.get('/', (req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello from from Primus Learning, this was awesome';
+  const msg = 'Hello from from Eniyan and Paramesh. Hey DevSecOps, here we are';
   res.send(getPage(msg));
 });
 
